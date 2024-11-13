7635d8254db6590ce5e18e51b08d7ed9627554c7
Felix <felix.berthier@epita.fr>
2024-11-13 16:00:13 +0100

    Added dark mode to container component

--- app/_components/container-landing.tsx
+++ app/_components/container-landing.tsx
@@ -10,7 +10,7 @@ export function ContainerLanding({
   return (
     <div className="relative isolate pt-14">
       <svg
-        className="absolute inset-0 -z-10 h-full w-full stroke-zinc-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+        className="absolute inset-0 -z-10 h-full w-full stroke-zinc-200 dark:stroke-zinc-700 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
         aria-hidden="true"
       >
         <defs>
@@ -35,13 +35,15 @@ export function ContainerLanding({
 
       <div className="sm:py-26 mt-20 px-6 py-16 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
-          <p className="text-base font-semibold leading-7 text-zinc-600">
+          <p className="text-base font-semibold leading-7 text-zinc-600 dark:text-zinc-400">
             {page}
           </p>
-          <h2 className="mt-2 text-4xl font-bold tracking-tight text-zinc-900 sm:text-6xl">
+          <h2 className="mt-2 text-4xl font-bold tracking-tight text-zinc-900 dark:text-zinc-200 sm:text-6xl">
             {title}
           </h2>
-          <p className="mt-6 text-lg leading-8 text-zinc-600">{description}</p>
+          <p className="mt-6 text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            {description}
+          </p>
         </div>
       </div>
     </div>
