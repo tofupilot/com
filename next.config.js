87298de016d9f038d124944f7490ed6c0696fae7
Felix <felix.berthier@epita.fr>
2024-11-13 15:34:11 +0100

    Replaced assetPrefix by basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
-  assetPrefix: "/blogs-static",
+  basePath: "/blog",
   images: {
     remotePatterns: [
       {
