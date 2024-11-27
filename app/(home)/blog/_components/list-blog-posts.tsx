7fa8f3cfaea6c5ceca49e9fc25bf67c1693b109d
Julien Buteau <julien@strake.one>
2024-11-27 15:36:23 +0100

    Fixed build

--- app/(home)/blog/_components/list-blog-posts.tsx
+++ app/(home)/blog/_components/list-blog-posts.tsx
@@ -2,6 +2,7 @@ import { urlForImage } from "@/app/(sanity)/lib/image";
 import Image from "next/image";
 import Link from "next/link";
 import { BadgeBlogCategory } from "./badge-blog-category";
+import { siteConfig } from "@/app/siteConfig";
 
 export default function ListBlogPosts({ posts }: { posts: any }) {
   return (
@@ -15,7 +16,7 @@ export default function ListBlogPosts({ posts }: { posts: any }) {
                 className="relative isolate flex flex-col gap-8 lg:flex-row"
               >
                 <Link
-                  href={`/${post.slug.current}`}
+                  href={siteConfig.baseLinks.blog + `/${post.slug.current}`}
                   className="relative aspect-[16/9]  hover:opacity-75 sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0 "
                 >
                   <Image
@@ -44,7 +45,11 @@ export default function ListBlogPosts({ posts }: { posts: any }) {
                   </div>
                   <div className="group relative max-w-xl">
                     <h3 className="mt-3 text-xl font-semibold leading-6 text-zinc-900 group-hover:text-zinc-600 dark:text-zinc-100 group-hover:dark:text-zinc-400">
-                      <Link href={`/${post.slug.current}`}>
+                      <Link
+                        href={
+                          siteConfig.baseLinks.blog + `/${post.slug.current}`
+                        }
+                      >
                         <span className="absolute inset-0" />
                         {post.title}
                       </Link>
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,13 @@
 import { Metadata } from 'next';
 
-import Benefits from './(home)/components/ui/Benefits';
-import Cta from './(home)/components/ui/Cta';
-import Features from './(home)/components/ui/Features';
-import Hero from './(home)/components/ui/Hero';
-import Infrastructure from './(home)/components/ui/Infrastructure';
-import Integrations from './(home)/components/ui/Integration';
-import LogoCloud from './(home)/components/ui/LogoCloud';
-import UserStories from './(home)/components/ui/UserStories';
+import Benefits from './components/ui/Benefits';
+import Cta from './components/ui/Cta';
+import Features from './components/ui/Features';
+import Hero from './components/ui/Hero';
+import Infrastructure from './components/ui/Infrastructure';
+import Integrations from './components/ui/Integration';
+import LogoCloud from './components/ui/LogoCloud';
+import UserStories from './components/ui/UserStories';
 
 export const metadata: Metadata = {
   title: 'Plug-and-play manufacturing test analytics',
