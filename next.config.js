291651f11f89a56e0594585c9528d9625cf6b4de
Julien Buteau <julien@strake.one>
2024-11-27 15:04:41 +0100

    Added backs docs redirect

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const docsUrl = "https://tofupilot-docs.vercel.app";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
@@ -10,6 +12,22 @@ const nextConfig = {
         pathname: "/**",
       },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/docs',
+        destination: `${docsUrl}/docs`,
+      },
+      {
+        source: '/docs/:path+',
+        destination: `${docsUrl}/docs/:path+`,
+      },
+      {
+        source: '/docs-static/_next/:path+',
+        destination: `${docsUrl}/docs-static/_next/:path+`,
+      },
+    ];
   },
 };
 
