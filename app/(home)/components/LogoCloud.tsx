9295e00e539dcd5d2758d1da19ccdb785fed7c36
Julien Buteau <julien.buteau@gmail.com>
2024-11-28 08:34:16 +0100

    Updated logo cloud

--- app/(home)/components/LogoCloud.tsx
+++ app/(home)/components/LogoCloud.tsx
@@ -1,4 +1,4 @@
-import { Logos } from './LogosCustomers';
+import { Logos } from "./LogosCustomers";
 
 export default function LogoCloud() {
   return (
@@ -6,10 +6,10 @@ export default function LogoCloud() {
       id="logo cloud"
       aria-label="Company logos"
       className="sm:mt-34 mt-24 flex animate-slide-up-fade flex-col items-center justify-center gap-y-6 text-center"
-      style={{ animationDuration: '1500ms' }}
+      style={{ animationDuration: "1500ms" }}
     >
       <p className="text-lg font-medium tracking-tighter text-gray-800 dark:text-gray-200">
-        Powering the best manufacturing test teams
+        Powering fast-moving manufacturing test teams
       </p>
       <div className="grid grid-cols-2 gap-10 gap-y-4 text-gray-900 md:grid-cols-4 md:gap-x-20 dark:text-gray-200">
         <Logos.Ecorobotix className="w-32" />
