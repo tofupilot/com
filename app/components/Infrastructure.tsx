45bb56deb0b14ca5aee2cf4bb6b6c68e40e743d6
Julien Buteau <julien.buteau@gmail.com>
2024-11-30 20:36:35 +0100

    Improved infrastructure section

--- app/components/Infrastructure.tsx
+++ app/components/Infrastructure.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import createGlobe from 'cobe';
-import { useEffect, useRef } from 'react';
+import createGlobe from "cobe";
+import { useEffect, useRef } from "react";
 
 export default function Infrastructure() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -39,19 +39,19 @@ export default function Infrastructure() {
 
   const features = [
     {
-      name: 'Enterprise Security',
+      name: "Enterprise Security",
       description:
-        'Best-in-class security practices keep your manufacturing data safe.',
+        "Best-in-class security practices keep your manufacturing data safe.",
     },
     {
-      name: 'Engineered For Scale',
+      name: "TofuPilot Graph Pipeline",
       description:
-        'Built for any volume of test data. From startups to global enterprises.',
+        "Designed for high volume real-time relational data processing.",
     },
     {
-      name: 'TofuPilot Graph Engine',
+      name: "Self-Hosting Option",
       description:
-        'Designed with a powerful relational core for native correlation detection.',
+        "Host TofuPilot in your infrastructure for when cloud is not an option.",
     },
   ];
 
@@ -59,7 +59,7 @@ export default function Infrastructure() {
     <div className="px-3">
       <section
         aria-labelledby="global-database-title"
-        className="relative mx-auto mt-28 flex w-full max-w-6xl flex-col items-center justify-center overflow-hidden rounded-3xl bg-zinc-950 pt-24 shadow-xl shadow-black/30 md:mt-40"
+        className="relative mx-auto mt-28 flex w-full max-w-6xl flex-col items-center justify-center overflow-hidden rounded-3xl bg-zinc-950 pt-24 shadow-xl dark:shadow-none shadow-black/30 md:mt-40"
       >
         <div className="absolute top-[17rem] size-[40rem] rounded-full bg-lime-800 blur-3xl md:top-[20rem]" />
         <div className="z-10 inline-block rounded-lg border border-lime-400/20 bg-lime-800/20 px-3 py-1.5 font-semibold uppercase leading-4 tracking-tight sm:text-sm">
@@ -82,12 +82,12 @@ export default function Infrastructure() {
           <div className="absolute bottom-0 h-3/5 w-full bg-gradient-to-b from-transparent via-zinc-950/95 to-zinc-950" />
           <div className="absolute inset-x-6 bottom-12 m-auto max-w-4xl md:top-2/3">
             <div className="grid grid-cols-1 gap-x-10 gap-y-6 rounded-lg border border-white/[3%] bg-white/[1%] px-6 py-6 shadow-xl backdrop-blur md:grid-cols-3 md:p-8">
-              {features.map(item => (
+              {features.map((item) => (
                 <div key={item.name} className="flex flex-col gap-2">
                   <h3 className="whitespace-nowrap bg-gradient-to-b from-lime-300 to-lime-500 bg-clip-text text-lg font-semibold text-transparent md:text-xl">
                     {item.name}
                   </h3>
-                  <p className="text-sm leading-6 text-lime-200/40">
+                  <p className="text-sm leading-6 text-zinc-500 dark:text-zinc-400">
                     {item.description}
                   </p>
                 </div>
