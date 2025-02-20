94a082ba1776837268cd26d79e55a9de3b5844a6
Julien Buteau <julien.buteau@gmail.com>
2025-02-20 10:15:25 +0100

    Improved metadata for templates page

--- app/(home)/templates/page.tsx
+++ app/(home)/templates/page.tsx
@@ -2,13 +2,27 @@ import { getAllTemplates } from "@/app/(sanity)/lib/client";
 import { ContainerLanding } from "@/app/components/ContainerLanding";
 import { TemplateGrid } from "@/app/components/ui/templates/TemplateGrid";
 import { Metadata } from "next";
+import { defaultKeywords, defaultOpenGraph } from "../metadata";
 
-export const revalidate = 60
+export const revalidate = 60;
 
 export const metadata: Metadata = {
   title: "Templates",
   description:
-    "Jumpstart your app development process with pre-built solutions from TofuPilot and our community.",
+    "Jumpstart your app development process with pre-built templates from TofuPilot and our community.",
+  keywords: [
+    ...defaultKeywords,
+    "TofuPilot Templates",
+    "App Development",
+    "Pre-built templates",
+    "Software Templates",
+  ],
+  openGraph: {
+    ...defaultOpenGraph,
+    title: "Templates - Pre-built templates for App Development | TofuPilot",
+    description:
+      "Jumpstart your app development process with pre-built templates from TofuPilot and our community.",
+  },
 };
 
 export default async function Page() {
