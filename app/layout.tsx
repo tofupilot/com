3d27504060afc0a5b57582e94ed9f57b1f7a822f
Julien Buteau <julien.buteau@gmail.com>
2025-02-20 08:52:45 +0100

    Moved layout element to landing page folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import "@/styles/tailwind.css";
 
-import { ThemeProvider } from "next-themes";
 import { Inter } from "next/font/google";
-import { Toaster } from "sonner";
 import { defaultMetadata } from "./(home)/metadata";
-import { CSPostHogProvider } from "./components/monitoring/posthog";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -14,7 +11,6 @@ const inter = Inter({
 
 export const metadata = defaultMetadata;
 
-// Empty layout for landing page + Sanity studio
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -26,14 +22,7 @@ export default function RootLayout({
       className={`${inter.className} scroll-auto antialiased`}
       suppressHydrationWarning
     >
-      <CSPostHogProvider>
-        <body className="min-h-screen selection:bg-zinc-100 selection:text-lime-700 dark:bg-zinc-950">
-          <ThemeProvider defaultTheme="dark" attribute="class">
-            {children}
-          </ThemeProvider>
-          <Toaster />
-        </body>
-      </CSPostHogProvider>
+      {children}
     </html>
   );
 }
