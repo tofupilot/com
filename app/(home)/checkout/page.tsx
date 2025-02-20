efa71f61721b194f0060abdc09b542f39df9c866
Julien Buteau <julien.buteau@gmail.com>
2025-02-20 11:46:10 +0100

    Migrated checkout page from app to com

---
+++ app/(home)/checkout/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Script from "next/script";
+
+/* Page handling redirection to checkout of Paddle transactions */
+export default function Page() {
+  return (
+    <Script
+      src="https://cdn.paddle.com/paddle/v2/paddle.js"
+      onLoad={() => {
+        // Use Vercel environment state to determine sandbox or production
+        const isSandbox =
+          process.env.NEXT_PUBLIC_VERCEL_ENV === "preview" ||
+          process.env.NEXT_PUBLIC_VERCEL_ENV === "development";
+
+        if (isSandbox) {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          Paddle.Environment.set("sandbox");
+        }
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        Paddle.Setup({
+          token: process.env.NEXT_PUBLIC_PADDLE_CLIENT_SIDE_TOKEN,
+          // logging
+          eventCallback: function (data: unknown) {
+            console.log(data);
+          },
+        });
+      }}
+    />
+  );
+}
