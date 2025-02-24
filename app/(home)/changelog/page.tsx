ee0c3a6f1f2128c820f5454e41ee5a16e53b1e7c
liam <huissoudliam@aol.com>
2025-02-24 17:53:24 +0100

    Added pagination on changelog page

--- app/(home)/changelog/page.tsx
+++ app/(home)/changelog/page.tsx
@@ -1,11 +1,28 @@
-import { getLastFiveReleases } from "@/app/(sanity)/lib/client";
+import {
+  getFromToReleases,
+  getNumberReleases,
+} from "@/app/(sanity)/lib/client";
 import { Release, ReleaseHighlight } from "@/app/(sanity)/schemas/release";
 import { ContainerLanding } from "../../components/ContainerLanding";
 import { CardRelease } from "../../components/ui/changelog/CardRelease";
 import { CardReleaseHighlight } from "../../components/ui/changelog/CardReleaseHighlight";
+import { PaginationTheme } from "../../components/ui/changelog/Pagination";
 
-export default async function Page() {
-  const releases = await getLastFiveReleases();
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const numberReleaseByPage = 5;
+  const searchParams = await props.searchParams;
+  const currentPage = Number(searchParams?.page) || 1;
+  const releases = await getFromToReleases(
+    { start: (currentPage - 1) * numberReleaseByPage },
+    { numberReleaseByPage }
+  );
+  const numberAllRelease = await getNumberReleases();
+  const totalPages = Math.round(numberAllRelease / numberReleaseByPage) + 1;
   return (
     <ContainerLanding
       title="Changelog"
@@ -15,7 +32,7 @@ export default async function Page() {
         <div className="mx-auto flex max-w-xl flex-col divide-y divide-zinc-200 pb-64 dark:divide-zinc-800">
           {releases &&
             releases.map((release: Release) => (
-              <div key={release._id}>
+              <div key={release._id} className="last:divide-0">
                 {release.highlights &&
                   release.highlights.map((highlight: ReleaseHighlight) => (
                     <CardReleaseHighlight
@@ -26,6 +43,7 @@ export default async function Page() {
                 <CardRelease key={release._id} {...{ release }} />
               </div>
             ))}
+          <PaginationTheme totalPages={totalPages} />
         </div>
       </div>
     </ContainerLanding>
