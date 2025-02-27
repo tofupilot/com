1c6cc43e851864d3008553f49dc763a80a4dabe1
Julien Buteau <julien.buteau@gmail.com>
2025-02-27 14:58:27 +0100

    Fixed prose dark mode

--- app/(home)/roadmap/_components/RoadmapList.tsx
+++ app/(home)/roadmap/_components/RoadmapList.tsx
@@ -58,7 +58,7 @@ export default function RoadmapList({ roadmaps }: { roadmaps: any }) {
                 {roadmap.title}
               </p>
 
-              <div className="mt-1 text-sm text-zinc-600 dark:text-zinc-400 prose">
+              <div className="mt-1 text-sm text-zinc-600 dark:text-zinc-400 prose dark:prose-invert">
                 {roadmap.description && (
                   <PortableText value={roadmap.description} />
                 )}
