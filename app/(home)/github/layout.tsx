c58a8763c2610c5b32e134407aff0975758d60cb
Julien Buteau <julien.buteau@gmail.com>
2025-03-06 21:41:18 +0100

    Update GitHub page layout to match Changelog metadata and structure

--- app/(home)/github/layout.tsx
+++ app/(home)/github/layout.tsx
@@ -1,7 +1,32 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+import { defaultKeywords, defaultOpenGraph } from "../metadata";
+
+export const metadata = {
+  title: 'Changelog',
+  description: 'Stay up to date with the latest TofuPilot features, improvements, and bug fixes. Read the changelog to see what's new.',
+  keywords: [
+    ...defaultKeywords,
+    'TofuPilot Updates',
+    'Changelog',
+    'New Features',
+    'Software Releases',
+    'Bug Fixes',
+    'Product Improvements',
+  ],
+  openGraph: {
+    ...defaultOpenGraph,
+    title: 'Changelog - TofuPilot Updates & New Features',
+    description: 'Stay up to date with the latest TofuPilot features, improvements, and bug fixes. Read the changelog to see what's new.',
+  },
+}
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <div className="relative overflow-hidden bg-white dark:bg-zinc-900">
+    <main role="main" className="">
       {children}
-    </div>
+    </main>
   );
 }
